3e5960246146b7ff107a89c045991dca72d16270
KKuzmane <kristinekuzmane@gmail.com>
2023-06-30 22:49:07 +0300

    Added memory database

---
+++ Cook-the-book/Data/DB_context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cook_the_book.Data
+{
+    public class DB_context: DbContext
+    {
+        public DB_context(DbContextOptions <DB_context>options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Cook-the-book/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿namespace Cook_the_book.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Ingridiants { get; set; }
+
+        public string Instructions { get; set; }
+
+        public List<string> Pictures { get; set; }
+
+        public List<string> KeyWords { get; set; }
+    }
+}
